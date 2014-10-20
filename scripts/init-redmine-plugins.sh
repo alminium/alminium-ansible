a93edfd40c131f47da1e571922db081f79a266a3
mikoto20000 <mikoto20000@gmail.com>
2014-10-21 08:06:35 +0900

    Add Redmine plugin installer.

---
+++ scripts/init-redmine-plugins.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+## setup plugin
+pushd .
+
+cd $INSTALL_DIR
+
+# for Hudson
+mv plugins/redmine_hudson/Gemfile plugins/redmine_hudson/Gemfile.org
+
+bundle install --path vendor/bundler --without development test postgresql sqlite
+
+bundle exec rake redmine:plugins:migrate
+bundle exec rake generate_secret_token
+bundle exec rake db:migrate
+bundle exec rake tmp:cache:clear
+bundle exec rake tmp:sessions:clear
+bundle exec rake redmine:plugins:process_version_change
+touch backlogs.dev
+bundle exec rake redmine:backlogs:install labels=false
+#bundle exec rake redmine:backlogs:install task_tracker=サポート story_trackers=機能 labels=false
+
+# FIXME: to avoid open_flash_chart assets aren't copied.
+cp -fr plugins/open_flash_chart/assets public/plugin_assets/open_flash_chart
+
+popd
+
