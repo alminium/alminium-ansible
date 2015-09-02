0275c498f9ea9eefba736336d196135f9e5609c0
mikoto20000 <mikoto20000@gmail.com>
2015-09-02 08:11:42 +0000

    Support CentOS7.1

--- roles/redmine/files/install-redmine.sh
+++ roles/redmine/files/install-redmine.sh
@@ -7,8 +7,8 @@ bundle install --path vendor/bundler --without development test postgresql sqlit
 bundle exec rake generate_secret_token
 bundle exec rake db:migrate RAILS_ENV=production
 echo $2 |bundle exec rake redmine:load_default_data RAILS_ENV=production
-gem install passenger --no-rdoc --no-ri
-passenger-install-apache2-module --auto
-passenger-install-apache2-module --snippet > /etc/httpd/conf.d/passenger.conf
+#gem install passenger --no-rdoc --no-ri
+#passenger-install-apache2-module --auto
+#passenger-install-apache2-module --snippet > /etc/httpd/conf.d/passenger.conf
 
 cp /opt/alminium/extra/svn/Redmine.pm /etc/httpd/Apache/Authn/
