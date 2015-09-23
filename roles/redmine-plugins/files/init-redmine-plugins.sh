82bc7be4e6911edba9a5554ea605806016bbf4bf
mikoto20000 <mikoto20000@gmail.com>
2015-09-23 00:42:16 +0000

    Fixed plugin install.

--- roles/redmine-plugins/files/init-redmine-plugins.sh
+++ roles/redmine-plugins/files/init-redmine-plugins.sh
@@ -3,13 +3,17 @@
 ## setup plugin
 pushd .
 
+echo $INSTALL_DIR override.
+
+INSTALL_DIR=/opt/alminium
+
 cd $INSTALL_DIR
 
 # for Hudson
 mv plugins/redmine_hudson/Gemfile plugins/redmine_hudson/Gemfile.org
 
 export RAILS_ENV=production
-bundle install --path vendor/bundler --without development test postgresql qlite
+bundle install --path vendor/bundler --without development test postgresql sqlite
 bundle exec rake redmine:plugins:migrate
 bundle exec rake generate_secret_token
 bundle exec rake db:migrate
