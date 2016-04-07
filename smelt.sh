550529329cfece47205a40fac0f8a6759c163a1c
mikoto20000 <mikoto20000@gmail.com>
2016-04-07 22:04:25 +0900

    Split Redmine server and Database server.

--- smelt.sh
+++ smelt.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 yum install -y iproute git epel-release
-yum install -y ansible
-ansible-playbook site.yml
+yum install -y python-pip libselinux-python
+pip install --upgrade ansible
+ansible-playbook site.yml -i hosts
